5e7432e58dd6ae6dd0ec4eef6bb4780a3de3e880
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-03-15 17:48:36 +0200

    Solve problem in user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin','company-owner','job-seeker'])->default('job-seeker');
+            $table->string('role')->default('job-seeker');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
